42fbd8f005f6f076ebfa9c5c952cf129108f4e28
Fábio Lira <fabioliradev@gmail.com>
2022-06-18 17:43:39 -0300

    padronizing names

--- target.scala
+++ target.scala
@@ -1,4 +1,4 @@
-object GreeterClass {
+object Greeter {
   def greeter(name: String): String = "kekw " + name
 
   def main(args: Array[String]) = args.map( (x:String) => println("hello, " + x + "!") )
