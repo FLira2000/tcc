6847c0a9c1a0be9269a398f5a6b2ce1b5f64a01b
Fábio Lira <fabioliradev@gmail.com>
2022-06-19 09:53:07 -0300

    adding jmh to profiling

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,21 @@
-object Main extends App{
+package tcc;
+
+import org.openjdk.jmh.annotations._
+import java.util.concurrent.TimeUnit
+
+
+@BenchmarkMode(Array(Mode.AverageTime))
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@State(Scope.Thread)
+class Main extends App{
   val names = List("songa", "monga", "minga")
-  names.map((name:String) => println(JavaGreeter.greet(name) + "from java!") )
-  names.map((name:String) => println(ScalaGreeter.greet(name) + "from scala!") )
+
+  @Benchmark
+  def test(): Unit = {
+    names.map((name:String) => println(JavaGreeter.greet(name) + "from java!") )  
+    names.map((name:String) => println(ScalaGreeter.greet(name) + "from scala!") )
+  }
 }
